901c4c1e4ac4b4a376de227eaeb2b77636485d71
ElizabethCA94 <ecarreno@unal.edu.co>
2020-03-28 00:46:17 -0500

    search with conditions 'male', 'female' and 'all'

--- js/script.js
+++ js/script.js
@@ -1,51 +1,65 @@
 var users = [{
         name: 'Lucy',
         gender: 'F',
-        hooby: 'pets',
+        hobby: 'pets',
         avatar: 'avatar1.png'
     },
     {
         name: 'Bety',
         gender: 'F',
-        hooby: 'pets',
+        hobby: 'pets',
         avatar: 'avatar2.png'
     },
     {
         name: 'Ronald',
         gender: 'M',
-        hooby: 'music',
+        hobby: 'music',
         avatar: 'avatar3.png'
     },
     {
         name: 'Cristhoper',
         gender: 'M',
-        hooby: 'sports',
+        hobby: 'sports',
         avatar: 'avatar4.png'
     },
 ];
 
 window.addEventListener('load', function(){
+    function search() {
+        {
+            //results.innerHTML = 'hola<br/>mundo <div>como estas</div>'; 
+            //get hobby
+            var hobbyField = document.getElementById('hobby');
+            var hobby = hobbyField.value;
+            console.log(hobby);
+            //get gender
+            var genderField = document.getElementById('gender');
+            var gender = genderField.options[genderField.selectedIndex].value;
+            console.log(gender);
+                
+            var resultsHTML = '';
+            var numUsers = users.length;
     
+            for(var i=0; i<numUsers; i++){
+
+                if(gender == 'A' || gender == users[i].gender){
+                    if(hobby == '' || hobby == users[i].hobby){
+                        resultsHTML += '<div class="person-row">\
+                                    <img src="images/'+users[i].avatar+'">\
+                                    <div class="person-info">\
+                                    <div> ' + users[i].name + '</div>\
+                                    <div> ' + users[i].hobby + '</div></div>\
+                                    <button>Add as friend</button></div>'; 
+                    }
+                    
+                }                   
+            }
+            results.innerHTML = resultsHTML;
+        }   
+    }
     var results = document.getElementById('results');
     var searchBtn = document.getElementById('searchBtn');
     
-    searchBtn.addEventListener('click', function(){
-        //results.innerHTML = 'hola<br/>mundo <div>como estas</div>'; 
-        //get hobby
-        var hobbyField = document.getElementById('hobby');
-        var hobby = hobbyField.value;
-        console.log(hobby);
-        //get gender
-        var genderField = document.getElementById('gender');
-        var gender = genderField.options[genderField.selectedIndex].value;
-        console.log(gender);
-            
-        var resultsHTML = '';
-        var numUsers = users.length;
-
-        for(var i=0; i<numUsers; i++){
-            resultsHTML = resultsHTML + ' ' + users[i].name;
-        }
-        results.innerHTML = resultsHTML;
-    });
+    searchBtn.addEventListener('click', search);
+    search();
 });
